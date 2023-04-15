fc545531e1ebb35e5102c8dc4a4312c5bd5f15fa
us4c0d3 <jwseok312@gmail.com>
2023-04-15 13:29:48 +0000

    fix: indent error

--- back/summary/Gpt.py
+++ back/summary/Gpt.py
@@ -14,17 +14,17 @@ section:dict = {'subtitle':str, 'content':str}
 class Gpt:
     def __init__(self, article:dict,temprature:float = 0.5, max_tokens:int = 200):
     # OpenAI API key
-    openai.api_key = tokens.gpt_key
-    # Parameters for the API request
-    self.model_engine = "gpt-3.5-turbo"  # GPT-3.5 Turbo
-    self.temperature = temprature
-    self.max_tokens = max_tokens
-    self.article = article
-    # self.message = ["role":"system", ]
-    # message to generate text from
-    for d in self.article['sections']:
-        text = d['content']
-        self.message.append({"role":"user", "content":text})
+        openai.api_key = tokens.gpt_key
+        # Parameters for the API request
+        self.model_engine = "gpt-3.5-turbo"  # GPT-3.5 Turbo
+        self.temperature = temprature
+        self.max_tokens = max_tokens
+        self.article = article
+        # self.message = ["role":"system", ]
+        # message to generate text from
+        for d in self.article['sections']:
+            text = d['content']
+            self.message.append({"role":"user", "content":text})
     
     def getresponse(self):
         # Call the OpenAI GPT API and get the response
