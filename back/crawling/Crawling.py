a0289ec938186b707b44177ac7c3e056537c9f14
Wooseok Jang <jwseok312@gmail.com>
2023-05-02 11:01:53 +0000

    feat: parse another urls

--- back/crawling/Crawling.py
+++ back/crawling/Crawling.py
@@ -29,62 +29,89 @@ class Crawling():
             return -1
 
     def get_article(self, url: str):
-        response = requests.get(url)
+        if 'https://edition.cnn.com/' in url:
+            response = requests.get(url)
 
-        if response.status_code == 200:
-            html = response.text
-            soup = BeautifulSoup(html, 'html.parser')
+            if response.status_code == 200:
+                html = response.text
+                soup = BeautifulSoup(html, 'html.parser')
+                
+                title = soup.find('div', 'headline').get_text()
+                body = soup.find('div', 'article__content')
+
+                # list the first level children of the body
+                children = body.findChildren(recursive=False)
+                sub_title = 'INTRO'
+                sub_contents = []
+                article = {
+                    'title': title,
+                    'sections': []
+                }
+                for child in children:
+                    if child.name == 'h2':
+                        article['sections'].append({
+                            'title': sub_title,
+                            'content': '\n'.join(sub_contents)
+                        })
+                        sub_title = child.get_text().strip()
+                        sub_contents = []
+
+                    elif child.name == 'p':
+                        sub_contents.append(child.get_text().strip())
+
+                article['sections'].append({
+                    'title': sub_title,
+                    'content': '\n'.join(sub_contents)
+                })
+                return article
+
+                # with open('raw.txt', 'w') as f:
+                #     f.write(json.dumps(article, indent='\t'))
+                # with open('result.txt', 'w') as f:
+                #     f.write('TITLE: {}\n'.format(article['title']))
+                #     for section in article['sections']:
+                #         f.write('SECTION: {}\n'.format(section['title']))
+                #         f.write('{}\n'.format(section['content']))
+            else: 
+                print(response.status_code)
+                return -1
             
-            title = soup.find('div', 'headline').get_text()
-            print(title)
-            body = soup.find('div', 'article__content')
+            
 
-            # list the first level children of the body
-            children = body.findChildren(recursive=False)
-            sub_title = 'INTRO'
-            sub_contents = []
-            article = {
-                'title': title,
-                'sections': []
-            }
-            for child in children:
-                if child.name == 'h2':
-                    article['sections'].append({
-                        'title': sub_title,
-                        'content': '\n'.join(sub_contents)
-                    })
-                    sub_title = child.get_text().strip()
-                    sub_contents = []
+        else:
+        # Not CNN url
+            response = requests.get(url)
 
-                elif child.name == 'p':
-                    sub_contents.append(child.get_text().strip())
+            if response.status_code == 200:
+                html = response.text
+                soup = BeautifulSoup(html, 'html.parser')
+                
+                title = soup.find('h1').get_text()
+                body = soup.find_all('p')
 
-            article['sections'].append({
-                'title': sub_title,
-                'content': '\n'.join(sub_contents)
-            })
-
-            with open('raw.txt', 'w') as f:
-                f.write(json.dumps(article, indent='\t'))
-            with open('result.txt', 'w') as f:
-                f.write('TITLE: {}\n'.format(article['title']))
-                for section in article['sections']:
-                    f.write('SECTION: {}\n'.format(section['title']))
-                    f.write('{}\n'.format(section['content']))
-
-            with open('raw.txt', 'w') as f:
-                f.write(json.dumps(article, indent='\t'))
-
-            return article
-
-        else :
-            print(response.status_code)
-            return -1
+                # list the first level children of the body
+                sub_title = 'INTRO'
+                sub_contents = []
+                article = {
+                    'title': title,
+                    'sections': []
+                }
+                for p in body:
+                    sub_contents.append(p.text)
+                article['sections'].append({
+                    'title': sub_title,
+                    'content': '\n'.join(sub_contents)
+                })
+                return article
+            else:
+                print(response.status_code)
+                return -1
+                
 
 
 if __name__ == '__main__':
     c = Crawling()
-    term = input()
-    data = c.search_article(term)
-    print(data[0]["url"])
-    print(c.get_article(data[0]["url"]))
+    # term = input()
+    # data = c.search_article(term)
+    # print(data[0]["url"])
+    print(c.get_article('https://www.foxnews.com/us/illinois-interstate-crash-involving-72-vehicles-leaves-six-dead-more-30-injured-horrific'))
