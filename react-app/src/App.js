b4cc4d327f13d1e3c262c973cbb6df524740758c
BusanGukbap <dong2857210@gmail.com>
2023-05-20 15:50:10 +0900

    makeLoginPage

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,35 +1,16 @@
 import React, { useState } from 'react';
 import Button from './Button';
 import InputBox from './InputBox';
+import LoginPage from './LoginPage';
 import OutputBox from './OutputBox';
 
 function App() {
   const [inputValue, setInputValue] = useState('');
   const [articles, setArticles] = useState([]);
   const [summary, setSummary] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleSignUp = async (id, pw) => {
-    const response = await fetch('http://localhost:5010/api/sign_up', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ id, pw }),
-    });
-    const result = await response.json();
-    console.log(result);
-  };
-  const handleSignIn = async (id, pw) => {
-    const response = await fetch('http://localhost:5010/api/sign_in', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ id, pw }),
-    });
-    const result = await response.json();
-    console.log(result);
-  };
+  // 키워드 전송 누름
   const handleSubmit = async (event) => {
     event.preventDefault();
     const response = await fetch(`http://localhost:5010/api/search_article?keyword=${inputValue}`);
@@ -38,64 +19,65 @@ function App() {
     setInputValue('');
     setSummary('');
   };
+  
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
+  
+  // 뉴스 헤드라인 클릭
   const handleHeadlineClick = async (url) => {
     const response = await fetch(`http://localhost:5010/api/get_summary?url=${url}`);
     const data = await response.json();
     setSummary(data.summary);
   };
+  
+  // 로그인 클릭
+  const handleLoginClick = () => {
+    setIsLoggedIn(false);
+  }
+  
+  // 로그인 성공 시
+  const handleSignIn = () => {
+    setIsLoggedIn(true);
+  }
+  
   return (
     <div>
-      <h1> Sign Up </h1>
-      <form onSubmit={(event) => {
-        event.preventDefault();
-        handleSignUp(event.target.id.value, event.target.pw.value);
-      }}>
-        <input type="text" name="id" placeholder="ID" />
-        <input type="password" name="pw" placeholder="PW" />
-        <button type="submit">Sign Up</button>
-      </form>
-
-      <h1> Sign In </h1>
-      <form onSubmit={(event) => {
-        event.preventDefault();
-        handleSignIn(event.target.id.value, event.target.pw.value);
-      }}>
-        <input type="text" name="id" placeholder="ID" />
-        <input type="password" name="pw" placeholder="PW" />
-        <button type="submit">Sign In</button>
-      </form>
-      
-      <h1> AIPaper </h1>
-      <form onSubmit={handleSubmit}>
-        <InputBox onChange={handleInputChange} value={inputValue} />
-        <Button type="submit" text="키워드 전송" />
-      </form>
-
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Headline</th>
-          </tr>
-        </thead>
-        <tbody>
-          {articles.map((article, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td onClick={() => handleHeadlineClick(article.url)}>{article.headline}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      {summary && (
-        <div>
-          <h2>Summary</h2>
-          <p>{summary}</p>
-        </div>
+      {isLoggedIn ? (
+        <>
+          <button onClick={handleLoginClick}>Logout</button>
+          <h1> AIPaper </h1>
+          <form onSubmit={handleSubmit}>
+            <InputBox onChange={handleInputChange} value={inputValue} />
+            <Button type="submit" text="키워드 전송" />
+          </form>
+    
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Headline</th>
+              </tr>
+            </thead>
+            <tbody>
+              {articles.map((article, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td onClick={() => handleHeadlineClick(article.url)}>{article.headline}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+    
+          {summary && (
+            <div>
+              <h2>Summary</h2>
+              <p>{summary}</p>
+            </div>
+          )}
+        </>
+      ) : (
+        <LoginPage  onSignIn={handleSignIn} />
       )}
     </div>
   );
---
+++ react-app/src/LoginPage.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+/**
+ * 회원가입 페이지
+ * @param {*} param0
+ * @returns JSX.Element
+ */
+
+const qs = (sel) => document.querySelector(sel);
+
+function LoginPage({ /* onSignUp, onSignIn */ }) {
+    const createHeader = (id, pw) => ({
+        method: "POST",
+        headers: {"Content-Type": "application/json",},
+        body: JSON.stringify({ id, pw }),
+    })
+
+    const preventEvent = (handler) => (event) => {
+        event.preventDefault();
+        handler(event);
+    }
+
+    const handleSignUp = async (id, pw) => {
+        const response = await fetch("http://localhost:5010/api/sign_up", createHeader(id, pw));
+        const result = await response.json();
+        console.log(result);
+    };
+
+    // 로그인
+    const handleSignIn = async (event) => {
+        console.log("fetch");
+        const id = qs('input[name="sign-in-id"]');
+        const pw = qs('input[name="sign-in-pw"]');
+        id.value = pw.value = '';
+        
+        const response = await fetch("http://localhost:5010/api/sign_in", createHeader(id.value, pw.value));
+        const result = await response.json();
+        console.log(result);
+        // onSignIn(); // 부모 컴포넌트의 로그인 성공 핸들러 호출
+    }
+
+    return (
+        <div>
+            <h1> Sign Up </h1>
+
+            <form
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    // handleSignUp(id, pw);
+                }}
+            >
+                <input type="text" name="id" placeholder="ID" />
+                <input type="password" name="pw" placeholder="PW" />
+                <button type="submit">Sign Up</button>
+            </form>
+
+            <h1> Sign In </h1>
+            <form
+                onSubmit={preventEvent(handleSignIn)}
+            >
+                <input type="text" name="sign-in-id" placeholder="ID" />
+                <input type="password" name="sign-in-pw" placeholder="PW" />
+                <button type="submit">Sign In</button>
+            </form>
+        </div>
+    );
+}
+
+export default LoginPage;
