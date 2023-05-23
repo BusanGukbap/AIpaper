e05f30584ed6e7d8adbba8b680ee9f15305d599f
us4c0d3 <jwseok312@gmail.com>
2023-05-24 02:54:07 +0900

    fix: fix indentation

--- back/server/auth.py
+++ back/server/auth.py
@@ -14,27 +14,31 @@ def sign_up():
         pw = request.form['pw']
         uid = db_access.generate_uid()
 
-    try:
-        if db_access.get_user_by_id(id):
-            return jsonify({'success': False, 'message': '이미 존재하는 아이디입니다.'})
+        try:
+            if db_access.get_user_by_id(id):
+                return jsonify({'success': False, 'message': '이미 존재하는 아이디입니다.'})
 
-    
-        if db_access.save_user(uid, id, pw):
-            return jsonify({'success': True})
-        else:
-            return jsonify({'success': False, 'message': '회원가입에 실패했습니다.'})
-    except Exception as e:
-        return jsonify({'success': False, 'message': str(e)})
+            if db_access.save_user(uid, id, pw):
+                return jsonify({'success': True})
+            else:
+                return jsonify({'success': False, 'message': '회원가입에 실패했습니다.'})
+                
+        except Exception as e:
+            return jsonify({'success': False, 'message': str(e)})
 
 def sign_in():
     if request.method == 'POST':
         id = request.form['id']
         pw = request.form['pw']
 
-        user = db_access.get_user_for_check(id, pw)
+        try:
+            user = db_access.get_user_for_check(id, pw)
 
-        if user:
-            session['user'] = user
-            return jsonify({'success': True, 'uid': user.uid})
-        else:
-            return jsonify({'success': False, 'message': '아이디 또는 비밀번호가 틀렸습니다.'})
+            if user:
+                session['user'] = user
+                return jsonify({'success': True, 'uid': user.uid})
+            else:
+                return jsonify({'success': False, 'message': '아이디 또는 비밀번호가 틀렸습니다.'})
+
+        except Exception as e:
+            return jsonify({'success': False, 'message': str(e)})
