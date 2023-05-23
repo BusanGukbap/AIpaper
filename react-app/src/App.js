cd77283e9c12db96bd7e1025a5fa13bce55f4eba
kalteon <102862962+kalteon@users.noreply.github.com>
2023-05-23 18:11:11 +0900

    feat: set background of translatedText

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,11 +6,31 @@ import OutputBox from './OutputBox';
 function App() {
   const [inputValue, setInputValue] = useState('');
   const [articles, setArticles] = useState([]);
-  const [summary, setSummary] = useState(' this is a summary');
+  const [summary, setSummary] = useState(`A "counter-terrorist operation" was ongoing in southwestern Russia's Belgorod region, its governor said Tuesday, a day after a town bordering Ukraine was attacked in an incursion claimed by pro-Ukraine Russian nationals.
+
+  “The Defense Ministry continues to sweep the territory with the help of law enforcement agencies,” Belgorod Gov. Vyacheslav Gladkov said on Telegram.
+  “All the necessary actions are being taken by the security forces. We are waiting for the completion of the counter-terrorist operation that was announced yesterday. I will try to update the information for you as quickly as possible.”
+  Earlier, Gladkov said Belgorod was hit by drone attacks overnight, following an incursion Monday claimed by Russians partisans aligned with the Ukrainian army.
+  
+  The Freedom of Russia Legion and Russian Volunteer Corps earlier said they had "fully liberated the settlement of Kozinka" and "entered Grayvoron," after crossing from Ukraine into Belgorod on Monday.
+  
+  On Tuesday, Gladkov urged Grayvoron residents not to return to their homes until the operation was completed.`);
 
   const [trnaslatedText, setTranslatedText] = useState('');
   const [tooltipStyle, setTooltipStyle] = useState({});
 
+  const handledifficulty = async (difficulty) => {
+    const response = await fetch(`http://localhost:5010/api/difficulty`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ difficulty })
+    });
+    const data = await response.json();
+    console.log(data.result);
+    setSummary(data.result);
+  };
   const handleTranslate = async (event) => {
     console.log("mouseup");
     const selectedText = window.getSelection().toString();
@@ -31,12 +51,12 @@ function App() {
         position: 'fixed',
         top: event.clientY + 'px',
         left: event.clientX + 'px',
+        backgroundColor: 'white',
+        opacity: 0.9,
       });
     }
-    //setTranslatedText('');
-    //setTooltipStyle({});
   };
-  const handleClick = () => {
+  const handleTranslatedTextClick = () => {
     setTranslatedText('');
     setTooltipStyle({});
   };
@@ -129,8 +149,10 @@ function App() {
           <h2>Summary</h2>
           <p onMouseUp={handleTranslate}>{summary}</p>
           {trnaslatedText && (
-            <div style={tooltipStyle} onClick={handleClick}>{trnaslatedText}</div>
+            <div style={tooltipStyle} onClick={handleTranslatedTextClick}>{trnaslatedText}</div>
           )}
+          <button onClick={() => handledifficulty("ease")}>easy</button>
+          <button onClick={() => handledifficulty("difficulty")}>difficult</button>
         </div>
       )}
     </div>
