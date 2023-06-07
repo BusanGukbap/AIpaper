ea33ceefdf12db9817342870aa8e5f0dbc98d6a0
BusanGukbap <dong2857210@gmail.com>
2023-06-07 21:18:29 +0900

    modify title.js

--- react-app/src/Pages/Title.js
+++ react-app/src/Pages/Title.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation, Link } from "react-router-dom";
-import { Button, Row, Col, Nav, Container, Spinner, Table } from 'react-bootstrap';
+import { Button, Row, Col, Table, ButtonGroup, Card, Placeholder, Alert, Spinner } from 'react-bootstrap';
 
 function TitlePage({}) {
   const navigate = useNavigate();
   const location = useLocation();
   const [isSpinner, setIsSpinner] = useState(false);
   const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [inputValue, setInputValue] = useState('');
 
   const goToHome = () => {
@@ -37,6 +38,7 @@ function TitlePage({}) {
   };
 
   const handleHeadlineClick = async (article) => {
+    setIsLoading(true);
     const response = await fetch(`http://localhost:5010/api/summary`, {
       method: 'POST',
       headers: {
@@ -46,6 +48,8 @@ function TitlePage({}) {
     });
     
     const result = await response.json();
+    setIsLoading(false);
+    navigate("/Summary", {state : {a : result}})
     console.log(result);
     //setSummary(result.summary);
     //console.log(summary);
@@ -63,22 +67,45 @@ function TitlePage({}) {
               <Spinner variant="primary" animation="border" style={{  width: '35px', height: '35px'}} /> :
               <Button type="submit" style={{ width: '150px', height: '50px', fontSize: '20px', marginBottom: '5px' }}>Search</Button>}
           </form> 
-          <Table striped border hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Headline</th>
-            </tr>
-          </thead>
-          <tbody>
-            {articles.map((article, index) => (
-              <tr key={index}>
-                <td>{index + 1}</td>
-                <td onClick={() => handleHeadlineClick(article)}>{article.headline}</td>
-              </tr>
-            ))}
-          </tbody>
-          </Table>
+          {isLoading ? (
+            <div>
+              <Alert variant="info" >
+                내용을 불러오는 데 약 30초 정도 걸립니다. 잠시만 기다려 주세요.
+              </Alert>
+              <Card border="dark">
+                <Card.Body>
+                  <Placeholder as={Card.Header} animation="wave">
+                    <Placeholder style={{ width: '100%', height: '50px' }}/>
+                  </Placeholder>
+                  <Placeholder as={Card.Text} animation="wave">
+                    <Placeholder style={{ width: '100%', height: '500px' }}/>
+                  </Placeholder>
+                  <ButtonGroup className="d-flex">
+                    <Placeholder.Button variant="info"/>
+                    <Placeholder.Button variant="dark"/>
+                    <Placeholder.Button variant="danger"/>
+                  </ButtonGroup>
+                </Card.Body>
+              </Card>
+            </div>
+            ) : (
+              <Table striped border hover>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Headline</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {articles.map((article, index) => (
+                    <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td onClick={() => handleHeadlineClick(article)}>{article.headline}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            )}
           </Col>
         </Row>
     </div>
