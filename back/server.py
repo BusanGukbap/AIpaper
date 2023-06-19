a9212c7644090df7cd02b30c546d42d8ae54985f
us4c0d3 <jwseok312@gmail.com>
2023-06-19 16:07:37 +0900

    add: add host and port

--- back/server.py
+++ back/server.py
@@ -151,4 +151,4 @@ def history():
 if __name__ == '__main__':
     ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
     ssl_context.load_cert_chain('/etc/letsencrypt/live/aipaper.site/fullchain.pem', '/etc/letsencrypt/live/aipaper.site/privkey.pem')
-    app.run(ssl_context=ssl_context)
+    app.run(host="0.0.0.0", port="5010", ssl_context=ssl_context)
